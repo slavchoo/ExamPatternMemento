0c7f3f434e0e888e402b22b3664039f70271135a
slavchoo <slavchoo2@gmail.com>
2013-01-06 14:16:43 +0300

    change package

---
+++ src/by/bsu/samples/memento/ControllerBad2.java
@@ -0,0 +1,32 @@
+package by.bsu.samples.memento;
+import java.util.Stack;
+/**
+ *
+ * @author training
+ */
+public class ControllerBad2 {
+    private Stack<GameStateBad2> undostates;
+	private Stack<GameStateBad2> redostates;
+    private GameStateBad2 currentstate;
+    public void addElement ( Integer x, Integer y ) {
+//...
+        undostates.push( currentstate );
+		redostates.clear();
+//...
+    }
+    public void Undo() {
+        if( !( undostates.isEmpty( ) ) )
+        {
+            redostates.push( currentstate );
+			currentstate = undostates.pop();
+        }
+    }
+	public void Redo() {
+        if( !( redostates.isEmpty( ) ) )
+        {
+			undostates.push( currentstate );
+            currentstate = redostates.pop();
+        }
+    }
+//...
+}
---
+++ src/by/bsu/samples/memento/IDraw.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package by.bsu.samples.memento;
+
+/**
+ *
+ * @author training
+ */
+public interface IDraw {
+    public void draw();
+}
